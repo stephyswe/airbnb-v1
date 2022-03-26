20b0cda942c307a8eae4b5f59f980804b3da1ea4
Stephanie <stephaniiehallberg@gmail.com>
2022-03-26 23:31:24 +0100

    chore(upgrade): server version upgrade

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,30 @@
-require("dotenv").config();
-
 import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import { connectDatabase } from "./database";
 import { typeDefs, resolvers } from "./graphql";
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db })
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+    context: () => ({ db }),
   });
 
-  server.applyMiddleware({ app, path: "/api" });
+  server.start().then(() => {
+    server.applyMiddleware({ app, path: "/api" });
+  });
+
   app.listen(process.env.PORT);
 
-  console.log(`[app] : http://localhost:${process.env.PORT}`);
+  console.log(`[app]: http://localhost:${process.env.PORT}`);
+
+  // # Example: Accessing the database
+  // const listings = await db.listings.find({}).toArray();
+  // console.log(listings);
 };
 
 mount(express());
-
-// Note: You will need to introduce a .env file at the root of the project
-// that has the PORT, DB_USER, DB_USER_PASSWORD, and DB_CLUSTER environment variables defined.
-// Otherwise, the server will not be able to start and/or connect to the database

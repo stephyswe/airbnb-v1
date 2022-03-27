20a43f7b6294f795abbecc7c754b345d59a0fad2
Stephanie <stephaniiehallberg@gmail.com>
2022-03-27 16:29:39 +0200

    feat(listing): The ListingCreateBooking React Component & mock listingBookingsElement

--- client/src/sections/Listing/index.tsx
+++ client/src/sections/Listing/index.tsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { Col, Layout, Row } from "antd";
+import { Moment } from "moment";
 import { ErrorBanner, PageSkeleton } from "../../lib/components";
 import { LISTING } from "../../lib/graphql/queries";
 import { Listing as ListingData, ListingVariables } from "../../lib/graphql/queries/Listing/__generated__/Listing";
-import { ListingBookings, ListingDetails } from "./components";
+import { ListingCreateBooking, ListingBookings, ListingDetails } from "./components";
 
 const { Content } = Layout;
 
@@ -13,6 +14,8 @@ const PAGE_LIMIT = 3;
 
 export const Listing = () => {
   const [bookingsPage, setBookingsPage] = useState(1);
+  const [checkInDate, setCheckInDate] = useState<Moment | null>(null);
+  const [checkOutDate, setCheckOutDate] = useState<Moment | null>(null);
 
   const { id } = useParams();
 
@@ -42,9 +45,67 @@ export const Listing = () => {
   }
 
   const listing = data ? data.listing : null;
-  const listingBookings = listing ? listing.bookings : null;
+  //const listingBookings = listing ? listing.bookings : null;
 
   const listingDetailsElement = listing ? <ListingDetails listing={listing} /> : null;
+
+  const listingBookings = {
+    total: 4,
+    result: [
+      {
+        id: "5daa530eefc64b001767247c",
+        tenant: {
+          id: "117422637055829818290",
+          name: "User X",
+          avatar:
+            "https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100",
+          __typename: "User"
+        },
+        checkIn: "2019-10-29",
+        checkOut: "2019-10-31",
+        __typename: "Booking"
+      },
+      {
+        id: "5daa530eefc64b001767247d",
+        tenant: {
+          id: "117422637055829818290",
+          name: "User X",
+          avatar:
+            "https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100",
+          __typename: "User"
+        },
+        checkIn: "2019-11-01",
+        checkOut: "2019-11-03",
+        __typename: "Booking"
+      },
+      {
+        id: "5daa530eefc64b001767247g",
+        tenant: {
+          id: "117422637055829818290",
+          name: "User X",
+          avatar:
+            "https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100",
+          __typename: "User"
+        },
+        checkIn: "2019-11-05",
+        checkOut: "2019-11-09",
+        __typename: "Booking"
+      },
+      {
+        id: "5daa530eefc64b001767247f",
+        tenant: {
+          id: "117422637055829818290",
+          name: "User X",
+          avatar:
+            "https://lh3.googleusercontent.com/a-/AAuE7mBL9NpzsFA6mGSC8xIIJfeK4oTeOJpYvL-gAyaB=s100",
+          __typename: "User"
+        },
+        checkIn: "2019-11-10",
+        checkOut: "2019-11-11",
+        __typename: "Booking"
+      }
+    ]
+  } as any;
 
   const listingBookingsElement = listingBookings ? (
     <ListingBookings
@@ -55,12 +116,25 @@ export const Listing = () => {
     />
   ) : null;
 
+  const listingCreateBooking = listing ? (
+    <ListingCreateBooking
+      price={listing.price}
+      checkInDate={checkInDate}
+      setCheckInDate={setCheckInDate}
+      checkOutDate={checkOutDate}
+      setCheckOutDate={setCheckOutDate}
+    />
+  ) : null;
+
   return (
     <Content className="listings">
       <Row gutter={24} justify="space-between">
-        <Col>
+        <Col xs={24} lg={14}>
           {listingDetailsElement}
           {listingBookingsElement}
+        </Col>
+        <Col xs={24} lg={10}>
+          {listingCreateBooking}
         </Col>
       </Row>
     </Content>
